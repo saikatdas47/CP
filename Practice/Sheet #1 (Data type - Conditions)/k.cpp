76c46f532e86e976ec751f65fa73cc3c35be654d
Saikat Das <saikatdasmain47@gmail.com>
2025-12-16 12:51:36 +0600

    ip

---
+++ Practice/Sheet #1 (Data type - Conditions)/k.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    vector<int> a(3);
+
+    for (int i = 0; i < 3; i++)
+    {
+        cin >> a[i];
+    }
+   sort(a.begin(), a.end());
+
+   cout<<a[0]<<" "<<a[2];
+
+    return 0;
+}
